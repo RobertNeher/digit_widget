39436df14234d0f2525f10790323b4ba369e75bc
Robert Neher <robert.neher@gmail.com>
2021-12-17 12:36:15 +0100

    Parameter for enforced dynamicRange and displaySize added

--- lib/multi_digit_display.dart
+++ lib/multi_digit_display.dart
@@ -2,41 +2,47 @@ import 'package:flutter/material.dart';
 import 'digit.dart';
 import 'digit_spacer.dart';
 
-Widget multiDigitDisplay(int number, {int radix = 10}) {
+Widget multiDigitDisplay(int number,
+    {int radix = 10, int digitCount = 0, bool dynamicRange = true}) {
   String _number = number.toRadixString(radix).toUpperCase();
-  final int displaySize = _number.length - 1;
 
   return Container(
       // padding: const EdgeInsets.all(8.0),
       child: Row(
-    children: getDigits(_number),
+    children: getDigits(_number, radix, digitCount, dynamicRange),
   ));
 }
 
-List<Widget> getDigits(String number) {
-  final int displaySize = number.length - 1;
+List<Widget> getDigits(
+    String number, int radix, int digitCount, bool dynamicRange) {
+  int displaySize;
+  bool negativeNumber = (int.tryParse(number)! < 0 && radix == 10);
   List<Widget> digitRow = <Widget>[];
-  RegExp all = RegExp(r'^[0-9A-F]*$');
-  RegExp onlyNumbers = RegExp(r'^[0-9]*$');
-  // minus symbol for negative numbers
-  if (onlyNumbers.hasMatch(number) && int.tryParse(number)! < 0) {
+  RegExp onlyNumbers = RegExp(r'^[0-9\-]*$');
+
+  displaySize = (dynamicRange && digitCount < number.length)
+      ? digitCount - 1
+      : number.length - 1;
+
+  // minus symbol for negative numbers of radix 10
+  if (negativeNumber) {
     digitRow.add(Digit('-'));
   }
-
-  for (int i = 0; i <= displaySize; i++) {
+  // now the numbeere itself
+  for (int i = negativeNumber ? 1 : 0; i <= displaySize; i++) {
     digitRow.add(Digit(number[i]));
     digitRow.add(DigitSpacer());
   }
 
-  for (int i = digitRow.length; i >= 2; i--) {
-    if (i > 3 && i % 3 == 0) {
-      digitRow.insert(
-          i,
-          DigitSpacer(
-            thousandGroup: true,
-          ));
-    }
-  }
+  // for (int i = digitRow.length; i >= 2; i--) {
+  //   if (i > 3 && i % 3 == 0) {
+  //     digitRow.insert(
+  //         i,
+  //         DigitSpacer(
+  //           thousandGroup: true,
+  //         ));
+  //   }
+  // }
 
   return digitRow;
 }
